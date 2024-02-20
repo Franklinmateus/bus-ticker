29edfc451846264598e2e62688cf8af992f16569
Franklin <frankmateus660@gmail.com>
2024-02-20 10:48:37 -0300

    Creates trip fragment (partial)

---
+++ app/src/main/java/com/cleios/busticket/ui/DriverTripsFragment.java
@@ -0,0 +1,41 @@
+package com.cleios.busticket.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import com.cleios.busticket.databinding.FragmentDriverHomeBinding;
+import com.cleios.busticket.databinding.FragmentDriverTripsBinding;
+import com.cleios.busticket.viewmodel.HomeViewModel;
+import org.jetbrains.annotations.NotNull;
+
+public class DriverTripsFragment extends Fragment {
+    private FragmentDriverTripsBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentDriverTripsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
