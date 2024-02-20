f682c6e90ef397fb213ad7310782267c9412771f
Paulo <paulosergio.ads1@gmail.com>
2024-02-20 11:10:45 -0300

    Setup navigation

--- app/src/main/java/com/cleios/busticket/ui/DriverHomeFragment.java
+++ app/src/main/java/com/cleios/busticket/ui/DriverHomeFragment.java
@@ -8,6 +8,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 import com.cleios.busticket.databinding.FragmentDriverHomeBinding;
 import com.cleios.busticket.viewmodel.HomeViewModel;
 import com.cleios.busticket.viewmodel.LoginViewModel;
@@ -37,6 +39,7 @@ public class DriverHomeFragment extends Fragment {
     public void onViewCreated(@NonNull @NotNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         binding.btnLogout.setOnClickListener(v -> logout());
+        binding.myTravels.setOnClickListener(v -> NavHostFragment.findNavController(this).navigate(DriverHomeFragmentDirections.actionDriverHomeFragmentToDriverTripsFragment()));
     }
 
     @Override
